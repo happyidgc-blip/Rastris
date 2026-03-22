e3215b4b22bff5d65dd71bd6f6fa66966bf87eb6
happyidgc-blip <happyidgc@gmail.com>
2026-03-22 18:20:08 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+BOT_TOKEN = "8493211054:AAGs7vffZ7ONzz43XiErMbLeONMSbqSLElU"
+UPI_ID = "your-upi@okhdfcbank"  # CHANGE TO YOUR UPI ID
+
+# Admin IDs
+ADMIN_IDS = [7167704900]  # @ZenoRealWebs
+
+# Prices
+PRICES = {
+    'fresh': 15,
+    'prepaid': 60,
+    'old': 40,
+    'igcc': 120
+}
+
+# Default password
+DEFAULT_PASSWORD = "111777"
